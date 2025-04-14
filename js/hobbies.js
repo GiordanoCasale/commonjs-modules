171252144dcfa6ad0886a28fb77395ae8df06ba9
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-14 16:36:14 +0200

    [UPD] hobbies.js function created

--- js/hobbies.js
+++ js/hobbies.js
@@ -1,4 +1,7 @@
 //creo la funzione
-const hobbies = (hobbyOne, hobbyTwo, hobbyThree) =>{
-    return 
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+    return [hobbyOne, hobbyTwo, hobbyThree];
 }
+
+//esporto la funzione
+module.exports= hobbies;
--- js/people.js
+++ js/people.js
@@ -1,5 +1,6 @@
 //vado a importare le funzioni
 const names = require('./names');
+const hobbies = require("./hobbies")
 //vado a dichiarare una variabile per riportare i valori
 const Nomi = names("Giordano ", "Casale");
 console.log(Nomi)
